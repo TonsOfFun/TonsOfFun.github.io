7924ae6cd513c98685d4356089c6a5bd47963743
Justin Bowen <JusBowen@gmail.com>
2023-02-02 21:18:39 -0800

    Update bundle.js

--- src/bundle.js
+++ src/bundle.js
@@ -3,54 +3,56 @@
 const app = new smarthome.App("1.0.0");
 app.onIdentify((request) => {
   console.debug("IDENTIFY request:", request);
-  const devices = request.inputs[0].payload.devices;
-  return Promise.all(devices.map((device) => {
-      const response = {
-          intent: smarthome.Intents.IDENTIFY,
-          requestId: request.requestId,
-          payload: {
-              device: {
-                  id: device.id || "",
-                  verificationId: "local-elgato-device-id"
+  return new Promise((resolve, reject) => {
+      const devices = request.inputs[0].payload.devices;
+      const responses = devices.map((device) => {
+          return {
+              intent: smarthome.Intents.IDENTIFY,
+              requestId: request.requestId,
+              payload: {
+                  device: {
+                      id: device.id || "",
+                      verificationId: "local-elgato-device-id"
+                  },
               },
-          },
-      };
-      console.debug("IDENTIFY response", response);
-      return response;
-  }));
+          };
+      });
+      console.debug("IDENTIFY response", responses);
+      resolve(responses);
+  });
 })
-.onQuery((request) => {
-    console.debug("QUERY request", request);
-    const devices = {};
-    const response = {
-        requestId: request.requestId,
-        payload: { devices: devices }
-    };
-    return Promise.all(request.inputs[0].payload.devices.map((device) => {
-        const command = new smarthome.DataFlow.HttpRequestData();
-        command.requestId = request.requestId;
-        command.method = smarthome.Constants.HttpOperation.GET;
-        command.deviceId = device.id;
-        command.port = 9123;
-        command.path = '/elgato/lights';
-        return app.getDeviceManager().send(command)
-            .then((result) => {
-            console.debug("QUERY response", result);
-            const httpResult = result;
-            const responseBody = httpResult.httpResponse.body;
-            const deviceState = JSON.parse(responseBody);
-            const lightState = deviceState.lights[0];
-            devices[device.id] = {
-                "on": !!lightState['on'],
-                "color": {
-                    "temperatureK": Math.floor(1e6 / lightState['temperature']),
-                },
-                "brightness": lightState['brightness'],
-            };
-        });
-    })).then(() => {
-        return response;
-    });
+  .onQuery((request) => {
+  console.debug("QUERY request", request);
+  const devices = {};
+  const response = {
+      requestId: request.requestId,
+      payload: { devices: devices }
+  };
+  return Promise.all(request.inputs[0].payload.devices.map((device) => {
+      const command = new smarthome.DataFlow.HttpRequestData();
+      command.requestId = request.requestId;
+      command.method = smarthome.Constants.HttpOperation.GET;
+      command.deviceId = device.id;
+      command.port = 9123;
+      command.path = '/elgato/lights';
+      return app.getDeviceManager().send(command)
+          .then((result) => {
+          console.debug("QUERY response", result);
+          const httpResult = result;
+          const responseBody = httpResult.httpResponse.body;
+          const deviceState = JSON.parse(responseBody);
+          const lightState = deviceState.lights[0];
+          devices[device.id] = {
+              "on": !!lightState['on'],
+              "color": {
+                  "temperatureK": Math.floor(1e6 / lightState['temperature']),
+              },
+              "brightness": lightState['brightness'],
+          };
+      });
+  })).then(() => {
+      return response;
+  });
 })
 .onExecute((request) => {
     console.debug("EXECUTE request", request);
